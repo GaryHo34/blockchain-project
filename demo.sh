ca7a8f3a5a78cc884caec6eeec7408dab582566a
Gary Ho <garyho0916@gmail.com>
2023-11-29 16:35:55 +0000

    Improve log message

--- demo.sh
+++ demo.sh
@@ -3,13 +3,13 @@
 echo "============== Scenario 1: Single Function Reentrancy Attack =============="
 for guard_name in "No Guard" "ReentrancyGuard" "ProxyGuard" "BalanceGuard"
     do
-        printf "\n\n========= Execute reentrancy attack on contract with $guard_name =========\n\n"
-        REENTRANCY_GUARD=$guard_name npx hardhat run scripts/singleFunctionReentrancyAttack.js 
+        printf "\n\n\n\n========= Testing $guard_name =========\n\n"
+        REENTRANCY_GUARD=$guard_name BALANCE=5 npx hardhat run scripts/singleFunctionReentrancyAttack.js 
     done
 
-printf "\n\n\n============== Scenario 2: Cross Function Reentrancy Attack =============="
+printf "\n\n\n\n============== Scenario 2: Cross Function Reentrancy Attack =============="
 for guard_name in "No Guard" "ReentrancyGuard" "ProxyGuard" "BalanceGuard"
     do
-        printf "\n\n========= Execute reentrancy attack on contract with $guard_name =========\n\n"
-        REENTRANCY_GUARD=$guard_name npx hardhat run scripts/crossFunctionReentrancyAttack.js 
+        printf "\n\n\n\n========= Testing $guard_name =========\n\n"
+        REENTRANCY_GUARD=$guard_name BALANCE=5 npx hardhat run scripts/crossFunctionReentrancyAttack.js 
     done
